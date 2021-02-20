575d037a60184169228c4dd0b0468e9ef715b09e
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-02-20 21:01:18 +0330

    add token encryption

--- Chayi/src/main/java/ir/coleo/chayi/constats/Constants.java
+++ Chayi/src/main/java/ir/coleo/chayi/constats/Constants.java
@@ -3,6 +3,15 @@ package ir.coleo.chayi.constats;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import static androidx.security.crypto.MasterKey.KeyScheme.AES256_GCM;
 
 /**
  * برای استفاده در اولین قسمت برنامه خود
@@ -14,8 +23,8 @@ import android.content.SharedPreferences;
  */
 public class Constants {
 
-    public static String VERSION_NAME = "0.11.0";
-    public static int VERSION_CODE = 38;
+    public static String VERSION_NAME = "0.11.2";
+    public static int VERSION_CODE = 40;
 
     public static String NO_TOKEN = "where is token";
     public static String TAG = "Constants chayi";
@@ -24,6 +33,8 @@ public class Constants {
     public static Context context = null;
     public static String TOKEN_STORAGE = "someWhereInDarkness";
     public static String TOKEN_DATA = "someWhereInDarkness12";
+    public static String TOKEN_STATE = "isTokenSavedEncrypted";
+    public static String TOKEN_STATE_VALUE = "isEncryptedToken";
     private static Class<?> restartActivity;
 
     public static Class<?> getRestartActivity() {
@@ -42,7 +53,31 @@ public class Constants {
      * گرفتن کلید ارتباط با سرور
      */
     public static String getToken() {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            SharedPreferences sharedPreferencesState = context.getSharedPreferences(TOKEN_STATE, Context.MODE_PRIVATE);
+            boolean encrypted_token = sharedPreferencesState.getBoolean(TOKEN_STATE_VALUE, true);
+            if (encrypted_token) {
+                try {
+                    MasterKey masterKeyAlias = new MasterKey.Builder(context).setKeyScheme(AES256_GCM).build();
+                    sharedPreferences = EncryptedSharedPreferences.create(context,
+                            TOKEN_STORAGE,
+                            masterKeyAlias,
+                            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
+                    Log.i(TAG, "setToken: EncryptedSharedPreferences ");
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (sharedPreferences == null) {
+                sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+            }
+        } else {
+            sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+        }
         return "Token " + sharedPreferences.getString(TOKEN_DATA, Constants.NO_TOKEN);
     }
 
@@ -50,7 +85,28 @@ public class Constants {
      * ذخیره کلید ارطباط با سرور در حافظه
      */
     public static void setToken(String token) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            try {
+                MasterKey masterKeyAlias = new MasterKey.Builder(context).setKeyScheme(AES256_GCM).build();
+                sharedPreferences = EncryptedSharedPreferences.create(context,
+                        TOKEN_STORAGE,
+                        masterKeyAlias,
+                        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
+                Log.i(TAG, "setToken: EncryptedSharedPreferences ");
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (sharedPreferences == null) {
+                sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+            }
+        } else {
+            sharedPreferences = context.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE);
+        }
+
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(TOKEN_DATA, token);
         editor.commit();
